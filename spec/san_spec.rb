74869b41b4d8ed9fd4601c1fa1a72c47b9c8d2e0
James Healy <james@yob.id.au>
2017-07-16 10:02:46 +1000

    update specs to work with rspec 3
    * use the 'expect' syntax
    * stop fiddling with the load path (bundler does that for us now)

--- spec/san_spec.rb
+++ spec/san_spec.rb
@@ -1,45 +1,42 @@
-$LOAD_PATH << File.dirname(__FILE__) + "/../lib"
-
-require 'spec'
 require 'san'
 
 describe "The SAN class" do
   it "should identify a valid SAN" do
-    SAN.new("9013725").valid?.should be_true
+    expect(SAN.new("9013725").valid?).to be_truthy
   end
 
   it "should identify a valid SAN" do
-    SAN.valid?("9013725").should be_true
-    SAN.valid?(9013725).should   be_true
-    SAN.valid?("902865X").should be_true
+    expect(SAN.valid?("9013725")).to be_truthy
+    expect(SAN.valid?(9013725)).to   be_truthy
+    expect(SAN.valid?("902865X")).to be_truthy
   end
 
   it "should identify an invalid SAN" do
-    SAN.valid?(nil).should       be_false
-    SAN.valid?("902865").should  be_false
-    SAN.valid?(Array).should     be_false
-    SAN.valid?(Array.new).should be_false
+    expect(SAN.valid?(nil)).to       be_falsey
+    expect(SAN.valid?("902865")).to  be_falsey
+    expect(SAN.valid?(Array)).to     be_falsey
+    expect(SAN.valid?(Array.new)).to be_falsey
   end
 
   it "should calculate a SAN check digit correctly" do
-    SAN.complete("901372").should eql("9013725")
-    SAN.complete(901372).should  eql("9013725")
+    expect(SAN.complete("901372")).to eql("9013725")
+    expect(SAN.complete(901372)).to  eql("9013725")
   end
 
   it "should correctly convert to a US based GLN" do
     # valid
-    SAN.new("9013725").to_us_gln.should eql("0799999013725")
-    SAN.new("9029761").to_us_gln.should eql("0799999029764")
+    expect(SAN.new("9013725").to_us_gln).to eql("0799999013725")
+    expect(SAN.new("9029761").to_us_gln).to eql("0799999029764")
 
     # invalid
-    SAN.new("9013724").to_us_gln.should be_nil
+    expect(SAN.new("9013724").to_us_gln).to be_nil
   end
 
   it "should correctly convert to a UK based GLN" do
     # valid
-    SAN.new("0159263").to_uk_gln.should eql("5030670159260")
+    expect(SAN.new("0159263").to_uk_gln).to eql("5030670159260")
 
     # invalid
-    SAN.new("9013724").to_uk_gln.should be_nil
+    expect(SAN.new("9013724").to_uk_gln).to be_nil
   end
 end
